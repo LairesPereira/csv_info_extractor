3a0970d8405a2370b6e4502a5f10dff375f864d3
Laires <lairespsoares@gmail.com>
2023-10-08 11:44:06 -0300

    criação de funções básicas de listagem
    listar todas as transacoes, listar todas as transacoes reais e listar transacoes especificas

---
+++ index.py
@@ -0,0 +1,68 @@
+from list_all_transactions import getTransactionsInfo
+
+all_transactions = getTransactionsInfo()
+
+# o parametro tipo de operações
+# devem ser sempre 'enviada' ou 'recebida'
+def all_transactions_values(list_of_transactions, type_of_operation):
+    total_sum = 0
+    for transaction in list_of_transactions:
+        if(f'{type_of_operation}' in transaction['type'].lower()):
+            total_sum += float(transaction['value'])
+    
+    return total_sum
+
+
+# algumas transacoes podem vir do proprio dono da conta
+# atraves de outras contas em seu nome, devemos ignorar
+# essas transações para termos o real montante de valor
+# recebido ou enviado. O parametro owner deve ter o nome registrado que 
+# está fazendo transacoes cruzadas
+def all_real_transaction_values(list_of_transactions, type_of_operation, owner):
+    total_sum = 0
+    crossed_transactions = 0
+    crossed_transactions_value = 0
+    
+    for transaction in list_of_transactions:
+        if('laires duarte soares' in transaction['name'].lower()): continue
+
+        if(not f'{owner}' in transaction['name'].lower()):    
+            if(f'{type_of_operation}' in transaction['type'].lower()):
+                total_sum += float(transaction['value'])
+       
+        elif((f'{owner}' in transaction['name'].lower()) and f'{type_of_operation}' in transaction['type'].lower()):        
+            crossed_transactions_value += float(transaction['value'])
+            crossed_transactions += 1
+    
+    return  {
+            'total_sum:': total_sum,
+            'crossed_transactions:': crossed_transactions,
+            'crossed_transactions_value: ': crossed_transactions_value,
+            }
+
+full_result = all_transactions_values(all_transactions, 'recebida')
+real_result_value = all_real_transaction_values(all_transactions, 'enviada', 'laires')
+real_result_recived = all_real_transaction_values(all_transactions, 'recebida', 'laires')
+# print(real_result_value)
+# print(real_result_recived)
+
+
+
+def target_transactions(list_of_transactions, type_of_operation, owner):
+
+    number_of_operations = 0
+    sum_of_target_operations = 0
+    for transaction in list_of_transactions:
+        if(type_of_operation in transaction['type'].lower()):
+            if(owner in transaction['name'].lower()):
+                sum_of_target_operations += float(transaction['value'])
+                number_of_operations += 1
+                print(transaction['name'], transaction['value'])
+
+    print(f'{number_of_operations} operações em {owner} somando:', sum_of_target_operations)
+    return sum_of_target_operations
+
+
+target_transactions(all_transactions, 'recebida', 'samya')
+
+
---
+++ list_all_transactions.py
@@ -0,0 +1,26 @@
+import csv
+
+# nubank_outubro_2023.csv
+
+# listando todas as transações
+# abrir o arquivo como objeto
+# para cada linha imprimimos apenas a informação relevante
+
+def getTransactionsInfo():
+    with open('./csv_files/nubank_outubro_2023.csv', 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile, delimiter=',')
+        final_list = []
+        for line in csv_reader:
+            if('Boleto' in line['Descrição']): continue
+            elif('fatura' in line['Descrição']): continue
+            else:
+                transaction_info = {
+                    'type': line['Descrição'].split('-')[0],
+                    'date': line['Data'],
+                    'value': line['Valor'],
+                    'name': line['Descrição'].split('-')[1]
+                }
+                final_list.append(transaction_info)    
+        return final_list
+
+
